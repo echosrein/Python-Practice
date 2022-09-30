933929198ce58d881707f48f6a3fdd5e56915e78
icho <echosrein@gmail.com>
2022-09-30 17:41:50 +0900

    9/30 파이썬 학습 (나도코딩)

---
+++ practice.py
@@ -0,0 +1,33 @@
+# print(5)
+# print('ballon')
+# print("butter")
+# print("z"*5)
+# print(5 > 10)
+# print(False)
+# print(False)
+
+# # 변수는 값을 저장하는 공간
+# name = "Bokdol"
+# animal = "dog"
+# age = 5
+# old = age > 5
+# # introduce your family
+# # print(" My " + animal + " s " + name + " is Bokdol ")
+# # print("He is", age, "yrs old")
+# # print("Bokdol is young? True")
+# #ctrl / 누르면 주석
+# 퀴즈
+# station = "사당"
+# print(station + "행 열차가 들어오고 있습니다")
+
+# print(2**2)  # 2^2
+# print(10 % 3)  # 나머지 구하기
+# print(10//2)  # 몫
+# print(4 >= 3)
+# print(3 == 3)  # 똑같다
+# print(3 != 4)  # !는 같지 않다
+# print(not (1 != 3))  # not은 반대
+# # and &, or | 임
+
+# 변수의 이름은 숫자로 시작할 수 없음.
+# int 정수, float 실수로>버림이 됨, str 문자로
